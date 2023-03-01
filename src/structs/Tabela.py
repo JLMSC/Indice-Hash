6b432a0465798c70079d5487e7de7a210629d574
JLMSC <reecaptcha@gmail.com>
2023-03-01 16:12:07 -0300

    Adicionado inserção de Tuplas e ponteiro entre Tuplas (Linked List)

--- src/structs/Tabela.py
+++ src/structs/Tabela.py
@@ -1,5 +1,29 @@
+"""Representa a estrutura Tabela.
+Contém todas as Tuplas construídas a partir do
+carregamento de algum arquivo de dados.
+"""
+
+from typing import List
+from structs.Tupla import Tupla # pylint: disable=import-error
+
 # TODO: Implementar a classe Tabela.
 class Tabela:
+    """Representa a estrutura Tabela."""
+    # Responsável pelo armazenamento das tuplas.
+    tuplas: List[Tupla] = []
+
     def __init__(self) -> None:
         pass
-    
+
+    def insert(self, tupla: Tupla) -> None:
+        """Insere um novo elemento na Tabela
+        e, também, faz com que a Tupla anterior
+        aponte para a nova Tupla inserida.
+
+        Args:
+            tupla (Tupla): A Tupla a ser registrada
+            na Tabela.
+        """
+        if len(self.tuplas) > 1:
+            self.tuplas[-1].proxima_tupla = tupla
+        self.tuplas.append(tupla)
--- src/structs/Tupla.py
+++ src/structs/Tupla.py
@@ -1,5 +1,21 @@
+"""Representa a estrutura Tupla.
+Contém o valor da chave e os dados da linha,
+representa uma linha na tabela.
+"""
+
+from typing import Any, TypeVar
+
 # TODO: Implementar a classe Tupla.
 class Tupla:
-    def __init__(self) -> None:
-        pass
-    
+    """Representa a estrutura Tupla."""
+    # Responsável pelo armazenamento dos registros.
+    dado: Any
+    # Aponta para o próximo elemento da lista (Linked List).
+    proxima_tupla: TypeVar("Tupla")
+    # Aponta para o índice da Tabela na Pagina.
+    indice_tabela: int
+
+    # TODO: Indice Tabela, alterar o indice da tabela quando a tabela for adicionado à pagina.
+
+    def __init__(self, dado: Any) -> None:
+        self.dado = dado
