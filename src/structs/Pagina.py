ef30fe98412f2db22cb6dc797fafe762c3ce3d1f
JLMSC <reecaptcha@gmail.com>
2023-03-03 01:06:04 -0300

    Encapsulamento de variáveis e adição de novos métodos.

--- src/structs/Pagina.py
+++ src/structs/Pagina.py
@@ -3,20 +3,22 @@ Estrutura de dados que representa a divisão
 e alocação física da tabela na mídia de armazenamento.
 """
 
-from typing import Dict, List
-from structs.Tabela import Tabela # pylint: disable=import-error
+# pylint: disable=import-error
 
-# TODO: Implementar a classe Pagina.
+from typing import Dict, List
+from structs.Tupla import Tupla
+from structs.Tabela import Tabela
+
+# FIXME: Revisar e testar.
+
 class Pagina:
     """Representa a estrutura Pagina."""
     # Tabelas contidas na Pagina.
-    tabelas: Dict[int, List[Tabela]] = {}
+    __paginas: Dict[int, List[Tupla]] = {}
     # Índice das Paginas, por padrão a primeira é 0.
-    indice_tabela_atual: int = 0
+    __indice_pagina_atual: int = 0
     # Tamanho fixo da Pagina.
-    tamanho_pagina: int
-
-    # TODO: O TableScan é aqui? ele faz em uma determinada Pagina ou na Tabela toda?
+    __tamanho_pagina: int
 
     def __init__(self, tamanho_pagina: int) -> None:
         """Inicializa uma Pagina com tamanho fixo.
@@ -25,9 +27,39 @@ class Pagina:
             tamanho_pagina (int): O tamanho da Pagina.
         """
         # Define o tamanho das Paginas.
-        self.tamanho_pagina = tamanho_pagina
+        self.__tamanho_pagina = tamanho_pagina
         # Inicializa a primeira Pagina.
-        self.tabelas[self.indice_tabela_atual] = []
+        self.__paginas[self.__indice_pagina_atual] = []
+
+    def get_page_fixed_size(self) -> int:
+        """Retorna o tamanho fixo das Paginas.
+
+        Returns:
+            int: O tamanho fixo das Paginas.
+        """
+        return self.__tamanho_pagina
+
+    def get_page_size(self, indice_pagina: int) -> int:
+        """Retorna a quantidade de Tuplas em uma Pagina.
+
+        Args:
+            indice_pagina (int): O índice da Pagina.
+
+        Returns:
+            int: A qntd. de Tuplas na Pagina.
+        """
+        return len(self.__paginas[indice_pagina])
+
+    def get_page_by_index(self, indice_pagina: int) -> List[Tupla]:
+        """Busca uma Pagina pelo seu índice e retorna-a.
+
+        Args:
+            indice_pagina (int): O índice da Pagina.
+
+        Returns:
+            List[Tupla]: O conteúdo (Tuplas) na Pagina.
+        """
+        return self.__paginas[indice_pagina]
 
     def insert(self, tabela: Tabela) -> None:
         """Insere todas as Tuplas de uma Tabela
@@ -37,12 +69,16 @@ class Pagina:
 
         Args:
             tabela (Tabela): A Tabela a ser obtido
-            as tuplas.
+            as tuplas para inserção nas Paginas.
         """
-        for tupla in tabela.tuplas:
-            if len(self.tabelas[self.indice_tabela_atual]) > self.tamanho_pagina:
-                self.indice_tabela_atual += 1
-                self.tabelas[self.indice_tabela_atual] = []
-            tupla.indice_tabela = self.indice_tabela_atual
-            self.tabelas[self.indice_tabela_atual].append(tupla)
+        for tupla in tabela.get_tuples():
+            # Passa para a próxima Pagina.
+            if len(self.__paginas[self.__indice_pagina_atual]) > self.get_page_fixed_size():
+                self.__indice_pagina_atual += 1
+                self.__paginas[self.__indice_pagina_atual] = []
+            # Adiciona uma Tupla à Pagina atual.
+            tupla.set_page_index(self.__indice_pagina_atual)
+            self.__paginas[self.__indice_pagina_atual].append(tupla)
+
+    # TODO: Precisa implementar "search" e "remove" ?
             
