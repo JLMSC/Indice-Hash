95879ca87b3952a9442e666590c365d8c70b0f84
JLMSC <reecaptcha@gmail.com>
2023-03-03 01:03:12 -0300

    Adicionado alguns exemplos e mais coisas p/ fazer.

--- src/Main.py
+++ src/Main.py
@@ -3,14 +3,14 @@ Ideias da aplicação:
     [X] Os registros serão armazenados em Tuplas.
     [X] As Tuplas serão armazenadas em Tabelas.
     [X] As Tabelas serão armazenadas em Paginas.
-    [ ] TableScan !
+    [X] TableScan !
     [ ] Os Bucket armazenarão Tuplas, i.e. seus
         registros, junto com um ponteiro para a
         Tabela (índice) da Pagina que a mesma se encontra. (Map)
         Ex:. Bucket 1 -> "Fulano": 1 (Índice da Pagina)
         ! O armazenamento nas Bucket deve ser feito, também,
         pela Função Hash.
-    [ ] A Função Hash determinará, dado uma chave
+    [X] A Função Hash determinará, dado uma chave
         qualquer, exemplo: "Fulano", em qual Bucket
         ela se encontrando, retornando então seu
         índice.
@@ -22,7 +22,7 @@ Ideias da aplicação:
              Bucket1("Fulano") -> 1 (Ponteiro para a Tabela 1 da Pagina)
     [ ] Depois de obtido o ponteiro/referência para a Tabela (índice) da Pagina,
         Buscar na Tabela da Pagina a chave alvo, e então retornar a Tupla.
-    [ ] Interface Gráfica!!!!
+    [ ] Interface Gráfica!!!! (Biblioteca tkinter)
 
     - Tabelas apontam para Tuplas
     - Tuplas apontam entre si (Linked List), será útil para um Table Scan (busca ordenada).
@@ -32,9 +32,12 @@ Ideias da aplicação:
     - Função Hash deve ter baixa dispersão.
 """
 
-from structs.Tupla import Tupla # pylint: disable=import-error
-from structs.Tabela import Tabela # pylint: disable=import-error
-from structs.Pagina import Pagina # pylint: disable=import-error
+# pylint: disable=import-error
+
+from structs.Tupla import Tupla
+from structs.Tabela import Tabela
+from structs.Pagina import Pagina
+from structs.Bucket import Bucket
 
 def read_input(path: str) -> Tabela:
     """Faz a leitura de dados de um arquivo,
@@ -63,12 +66,30 @@ def read_input(path: str) -> Tabela:
 
 def main() -> None:
     """Função Principal."""
-    # FIXME: Tabela, Tupla, Bucket, Pagina DEVEM ser variáveis globais.
+    # FIXME: Tabela, Tupla, Bucket, Pagina DEVEM ser variáveis globais (Facilitar o acesso na interface gráfica).
+
+    # FIXME: TODAS AS ESTRUTURAS DEVEM TER AS VARIÁVEIS PRIVADAS.
+
+    # FIXME: Usar o método hash_function de FuncaoHash quando for fazer uma busca/inserção nos Buckets.
+
+    # FIXME: Tamanho da Pagina DEVE ser passado DURANTE A INSTANCIAÇÃO DE PAGINA.
+
     # Pega a Tabela com todas as Tuplas registradas.
-    tabela = read_input(path="input.txt")
-    # TODO: Pedir o tamanho da Pagina ao usuário. (Interface Gráfica)
-    pagina = Pagina(tamanho_pagina=10)
+    tabela: Tabela = read_input(path="input.txt")
+
+    # Divide as Tuplas da Tabela em Paginas de tamanhos fixos.
+    pagina: Pagina = Pagina(tamanho_pagina=100)
     pagina.insert(tabela)
+
+    # # TODO: Exemplo de uma Table Scan (sem limite)
+    # result = tabela.table_scan("house")
+    # print(result)
+    # # TODO: Exemplo de uma Table Scan (com limite)
+    # result = tabela.table_scan("house", 31_415)
+    # print(result)
+
+    # TODO: Tem que fazer o Bucket ainda.
+    # bucket: Bucket = Bucket(len(tabela.get_size()))
     print("T")
 
 if __name__ == "__main__":
